6badfc95346310c3476ae1523e400cc05093141a
leonardotaverna <leonardotaverna@hotmail.com>
2023-05-01 16:34:12 -0300

    creo componenente NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>BlockBuster-X</h3>
+            <div>
+                <button>Películas</button>
+                <button>Series</button>
+                <button>Documentales</button>
+                <button>Programas de TV</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
