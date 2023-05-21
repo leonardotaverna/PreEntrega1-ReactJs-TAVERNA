5169bff858f0e3401218ffb608fe89620441bca3
leonardotaverna <leonardotaverna@hotmail.com>
2023-05-20 21:59:46 -0300

    genero componente ItemCount

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <ItemListContainer greeting={"Bienvenidxs a BlockBuster-X app"}/>     
+      <ItemListContainer greeting={"Bienvenidxs a BlockBuster-X app"}/>
+      <ItemCount initial={1} stock={5} onAdd={(quantity) => console.log("Items agregados:", quantity)}/>    
     </div>
   );
 }
