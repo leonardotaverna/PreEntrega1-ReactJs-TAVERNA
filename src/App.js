69fd31c847e53b4b66c52573d93b67d8dcedb897
leonardotaverna <leonardotaverna@hotmail.com>
2023-06-20 18:42:39 -0300

    creo CartContext

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import { CartProvider } from './context/cartContext';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path = "/" element = { <ItemListContainer/>}/>
-          <Route path = "/category/:categoryId" element = {<ItemListContainer />}/>
-          <Route path = "/item/:itemId" element = {<ItemDetailContainer/>}/>
-          <Route path = "*" element = {<h1>ERROR 404 Page not found</h1>}/>
-        </Routes>
+        <CartProvider>
+          <NavBar />
+          <Routes>
+            <Route path = "/" element = { <ItemListContainer/>}/>
+            <Route path = "/category/:categoryId" element = {<ItemListContainer />}/>
+            <Route path = "/item/:itemId" element = {<ItemDetailContainer/>}/>
+            <Route path = "*" element = {<h1>ERROR 404 Page not found</h1>}/>
+          </Routes>
+        </CartProvider>
       </BrowserRouter>
     </div>
   );
